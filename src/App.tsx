63575acdc7dd5ae7ca3687e7e2ba4b60aa9c38c3
muhammadsufiyanali <muhammadsufiyanali34@mail.com>
2026-02-01 20:29:22 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    console.log("===value");
+    const value = e.target.value;
+    setEmail(value);
+
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!regex.test(value)) {
+      setError("Invalid email");
+    } else {
+      setError("");
+    }
+  };
+
+  const passwordChange = (e) => {
+    console.log("===PassValue");
+    const PassValue = e.target.value;
+    setPassword(PassValue);
+
+    const passwordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+    if (!passwordregex.test(PassValue)) {
+      setError("Invalid password");
+    } else {
+      setError("");
+    }
+  
+
+  return (
+    <div className="container">
+      <h2 className="title">Login Form</h2>
+
+      <input
+        className="input"
+        type="text"
+        value={email}
+        onChange={handleChange}
+        placeholder="Enter email"
+      />
+
+      <input
+        className="input"
+        type="text"
+        value={password}
+        onChange={passwordChange}
+        placeholder="Enter password"
+      />
+
+      {error && <p className="error">{error}</p>}
+      <button>submit</button>
+    </div>
+  );
+}
+}
+
+export default App;
+
